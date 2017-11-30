d19174aed62866f1554ca04ee1a2e9c896286387
Vamsi Tallapudi <vamsi@coderefer.com>
2017-11-30 22:13:36 +0530

    Control Flows - When condition

--- src/main/ControlFlow/WhenEg.kt
+++ src/main/ControlFlow/WhenEg.kt
@@ -1,39 +1,42 @@
-/*
- * Copyright (c) 2017.
- * The Content is copyrighted to $Name
- */
-
 package main.ControlFlow
 
-import java.lang.Integer.parseInt
+// when is an alternative for switch in Java
 
 fun main(args: Array<String>) {
+    var x = 5
+    hasPrefix(5)
+    hasPrefix("vamsi")
+    hasPrefix(true)
 
+    performIfAlternative(x)
 
-    var x = 3
-    when (x){
-        1 -> print("x==1")
-        2 -> print("x==2")
-        3 -> print("x==3")
-        else -> print ("x is neither 1 or 2 or 3")
-    }
+    useWhenDirectly(x)
+}
 
-//    check in range
-    when (x) {
-        in 1..10 -> print("X is in range")
-        !in 10..20 -> print("X is not in range")
-    }
+fun useWhenDirectly(x:Int) {
+//    here when used as normal switch condition and not as an expression
+    when(x){
+        in 1..20 -> println("X is in between the specified values")
 
-//    when accepts expression too as condition as shown below
-    val s = "3"
-    when (s) {
-        x.toString() -> print("s encodes x")
-        else -> print("s does not encode x")
     }
 }
 
-// else ControlFlow is must for when used as expression
+// else is must when 'when' is used as expression
 fun hasPrefix(x:Any) = when (x) {
-    is String -> x.startsWith("va")
+    is String -> print(x.startsWith("va"))
+    is Int -> print(x.minus(1))
+    is Boolean -> print(x.not())
     else -> false
 }
+
+
+fun performIfAlternative(x:Int) {
+
+//    Here when is used as if else condition, so declaring a value is not required
+    when{
+        isOdd(x)-> println("Shut up!")
+        else -> println("same 2 u")
+    }
+}
+
+fun isOdd(a:Int) = a%2!=0
