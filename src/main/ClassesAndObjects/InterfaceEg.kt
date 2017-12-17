1780ee7b091adc9d3c849f894482009a115e3362
Vamsi Tallapudi <vamsi@coderefer.com>
2017-12-17 23:06:50 +0530

    Classes and Objects:
    1. Interfaces

---
+++ src/main/ClassesAndObjects/InterfaceEg.kt
@@ -0,0 +1,14 @@
+package main.ClassesAndObjects
+
+interface InterfaceEg{
+//    variable without initialization / abstract keyword (possible in interface)
+   var a:Int
+
+    //  method without body
+    fun apple()
+
+    //  method with body
+    fun mango(){
+        println("Mango")
+    }
+}
---
+++ src/main/ClassesAndObjects/InterfaceImplementation.kt
@@ -0,0 +1,22 @@
+package main.ClassesAndObjects
+
+fun main(args: Array<String>) {
+    var fruits  = Fruits()
+    fruits.apple()
+}
+
+class Fruits : InterfaceEg {
+    override var a: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
+
+    override fun apple() {
+        mango()
+        println("Apple is here!")
+    }
+
+    override fun mango() {
+        println("MAANGOOOO!!!!")
+        super.mango()
+    }
+}
