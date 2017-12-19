d3b057f8203e897f9a5fb7ac59a1d5c3fb55ea40
Vamsi Tallapudi <vamsi@coderefer.com>
2017-12-19 23:34:59 +0530

    Classes and Objects: Extension Function #2

--- src/main/classesandobjects/ExtensionFunctions.kt
+++ src/main/classesandobjects/ExtensionFunctions.kt
@@ -15,4 +15,16 @@ fun main(args: Array<String>) {
 //    and use it as if it were a function available within
 //    that class
     rabbit.snore() //--> Valid
+
+
+//    another example of Extension Function using collections from kotlin
+  fun MutableList<Int>.swap(index1:Int, index2:Int){
+      val tmp = this[index1]
+      this[index2] = this[index1]
+      this[index1] = tmp
+  }
+
+    val l = mutableListOf(1,2,3)
+    l.swap(0,2)
+
 }
