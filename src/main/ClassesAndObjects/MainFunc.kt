6ab698494152f46be4d978cee95496e830307c08
Vamsi Tallapudi <vamsi@coderefer.com>
2017-12-19 08:30:52 +0530

    Classes and Objects:
    1. Extension Function Example

--- src/main/ClassesAndObjects/MainFunc.kt
+++ src/main/ClassesAndObjects/MainFunc.kt
@@ -4,9 +4,20 @@ fun main(args: Array<String>) {
 //    var tiger = Tiger()
 //    tiger.sleep()
 
+
+//  Extension functions
+    fun Rabbit.snore(){
+        println("I am Rabbit, I am snoring")
+        walking =true
+    }
+
     var rabbit = Rabbit()
     rabbit.sleep()
 
-    var student = Student("Vamsi")
-    student.age
+//    power of extension functions-->
+//    we can declare a function for any class
+//    and use it as if it were a function available within
+//    that class
+    rabbit.snore() //--> Valid
+
 }
