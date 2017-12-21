ec81efcace57d27161af14476287c54006f8352f
Vamsi Tallapudi <vamsi@coderefer.com>
2017-12-21 08:34:57 +0530

    Overloading Member Functions

--- src/main/classesandobjects/MemberFuncVsExtenFunc.kt
+++ src/main/classesandobjects/MemberFuncVsExtenFunc.kt
@@ -8,10 +8,16 @@ class MemberFuncVsExtenFunc {
 }
 
 fun main(args: Array<String>) {
-    fun MemberFuncVsExtenFunc.foo(){
+    fun MemberFuncVsExtenFunc.foo() {
         println("Extension function wins!")//--> Never prints as it is shadowed by member function
     }
 
     var memFuncVsExtFunc = MemberFuncVsExtenFunc()
     memFuncVsExtFunc.foo()
+
+    //    Member function overloading
+    fun MemberFuncVsExtenFunc.foo(str:String) {
+        println("Overloading Member function from Extension Function by adding '$str' as parameter")
+    }
+    memFuncVsExtFunc.foo("Vamsi")
 }
