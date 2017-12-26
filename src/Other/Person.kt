d433e441c24e1275e60edb223ad879e2426273bc
Vamsi Tallapudi <vamsi@coderefer.com>
2017-12-26 09:49:07 +0530

    Data classes #2, copying, getters and setters etc.

---
+++ src/Other/Person.kt
@@ -0,0 +1,9 @@
+package Other
+
+class Human(name:String, age:Int, height:Int, weight:Int){
+
+}
+
+data class HumanBeing(var name:String, val age:Int, val height:Int, var weight:Int){
+
+}
--- src/main/classesandobjects/DataClasses.kt
+++ src/main/classesandobjects/DataClasses.kt
@@ -1,9 +1,28 @@
 package main.classesandobjects
 
+import Other.Human
+import Other.HumanBeing
+
 /*data class must have atleast one parameter in primary constructor*/
-data class User(override var name:String, override var age:Int):Student("Vamsi",27)
+data class User( var name:String, var age:Int) {
+
+}
 
 fun main(args: Array<String>) {
+
+//instantiating normal class
+    val human = Human("Samantha", 24, 5, 64)
+//    human.age //--> cannot do since no default getters / setters for class
+
+//    instantiating data class
+    val humanbeing = HumanBeing("Taylor Swift", 34, 6, 54)
+    humanbeing.age //--> accessible since getters and setters are generated for data class
+
     val user = User("Vamsi", 27)
+    val jack = User("Jack", 36)
+    /*copy function is used to create new object with just few data changes
+    * (here it is just age*/
+//    copy function is generated automatically when we create data class
+    val youngerJack = jack.copy(age = 5)
 
 }
