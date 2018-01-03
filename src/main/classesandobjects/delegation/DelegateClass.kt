08f18030c01d0f1b084d5603b278955c945c0a0a
Vamsi Tallapudi <vamsi@coderefer.com>
2018-01-03 21:24:27 +0530

    Delegate Eg

---
+++ src/main/classesandobjects/delegation/DelegateClass.kt
@@ -0,0 +1,16 @@
+package main.classesandobjects.delegation
+
+class Rectangle(val width: Int, val height: Int) {
+    fun area() = width * height
+}
+
+class Window(val bounds: Rectangle) {
+    // Delegation
+    fun area() = bounds.area()
+}
+
+fun main(args: Array<String>) {
+    val rect = Rectangle(15,20) //--> created rectangle object here
+    val win = Window(rect) // --> passed rectangle object ref to window object
+    println(win.area()) // --> indirectly accessing Rectangle's area() using Window's area
+}
