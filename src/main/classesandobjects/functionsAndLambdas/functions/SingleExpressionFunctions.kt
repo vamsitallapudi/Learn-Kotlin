ccfdfa1787b17c88364e5f13ec3275e0602541f1
Vamsi Tallapudi <vamsi@coderefer.com>
2018-01-07 12:43:03 +0530

    Functions and Lambdas #2 - SingleExpressionFunctions

---
+++ src/main/classesandobjects/functionsAndLambdas/functions/SingleExpressionFunctions.kt
@@ -0,0 +1,3 @@
+package main.classesandobjects.functionsAndLambdas.functions
+
+fun doubleTheValue(x:Int) = x * 2 //--> single expression at the simplest form. Here the return type is omitted and the curly braces are omitted too.
