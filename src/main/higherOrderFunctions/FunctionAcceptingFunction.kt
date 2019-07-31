0fe6a317e5c743e345302b7c6bf11513793d2e80
Vamsi Tallapudi <vamsi@coderefer.com>
2019-07-31 08:28:33 +0530

    higher order functions

--- src/main/higherOrderFunctions/FunctionAcceptingFunction.kt
+++ src/main/higherOrderFunctions/FunctionAcceptingFunction.kt
@@ -1,2 +1,7 @@
 package main.higherOrderFunctions
 
+fun main(args: Array<String>) {
+    val list = listOf(1,2,3,4,5)
+    list.forEach({a -> a*a})
+}
+
--- src/main/higherOrderFunctions/FunctionReturningFunction.kt
+++ src/main/higherOrderFunctions/FunctionReturningFunction.kt
@@ -1,2 +1,12 @@
 package main.higherOrderFunctions
 
+fun main(args: Array<String>) {
+    val areaOfSquare = area()
+    println(areaOfSquare(5))
+    println(areaOfSquare(10))
+}
+
+fun area() : (Int) -> Int {
+    return {i -> i * i}
+}
+
